baceee1696caabb612df57bd84d7c22c6c8d23fa
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:30 +0800

    2025年10月14日 星期二 15时57分30秒 CST

--- .make/depend.dir/libplatform.mk
+++ .make/depend.dir/libplatform.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libplatform.recursive
 ECHO_TARGETS += libplatform.echo
 libplatform.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libevent/               >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libevent/.make >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
-	cd /volumes/llama/home/alpine/libraries/libplatform/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libevent/               >> /Users/lidali/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libevent/.make >> /Users/lidali/alpine/libraries/libplatform/.make/super
+	cd /Users/lidali/alpine/libraries/libplatform/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libplatform/.make/super
 libplatform.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libevent/               >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libevent/.make >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
-	cd /volumes/llama/home/alpine/libraries/libplatform/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libevent/               >> /Users/lidali/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libevent/.make >> /Users/lidali/alpine/libraries/libplatform/.make/super
+	cd /Users/lidali/alpine/libraries/libplatform/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libplatform/.make/super
